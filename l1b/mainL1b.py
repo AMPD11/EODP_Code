cd17c89be551b8f9d6fe2f6b8d65b386c86fd43c
AMPD11 <andreimariuspopd@gmail.com>
2025-10-22 19:58:57 +0200

    Usar rutas con "/" para evitar '\U' en logging.config (unicodeescape)

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\\Users\\Andrei\\Documents\\MiSE\\3.1 Semester\\Earth Observation Data Processing\\eodpcode\\auxiliary'
-indir = r"C:\\Users\Andrei\\Documents\\MiSE\\3.1 Semester\\Earth Observation Data Processing\\data\\EODP-TS-L1B-20250925T181849Z-1-001\\EODP-TS-L1B\\input"
-outdir = r"C:\\Users\\Andrei\\Documents\\MiSE\\3.1 Semester\\Earth Observation Data Processing\\data\\EODP-TS-L1B-20250925T181849Z-1-001\\EODP-TS-L1B\\output"
+auxdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/eodpcode/auxiliary"
+indir  = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/data/EODP-TS-L1B/input"
+outdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/L1B_0001"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
