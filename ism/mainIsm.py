efe3016c8b6c2c9d7b0c5cc7bc64cdf4c64fdbf1
AMPD11 <andreimariuspopd@gmail.com>
2025-10-28 19:55:21 +0100

    slashes normales para evitar errores

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,9 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\\Users\\Andrei\\Documents\\MiSE\\3.1 Semester\\Earth Observation Data Processing\\eodpcode\\auxiliary'
-indir = r"C:\\Users\\Andrei\\Documents\\MiSE\\3.1 Semester\\Earth Observation Data Processing\\data\\EODP-TS-ISM\\input\\gradient_alt100_act150" # small scene
-outdir = r"C:\\Users\\Andrei\\Documents\\MiSE\\3.1 Semester\\Earth Observation Data Processing\\data\\EODP-TS-ISM\\output"
+auxdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/eodpcode/auxiliary"
+indir  = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/data/EODP-TS-ISM/input/gradient_alt100_act150"
+outdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/ISM_0001"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
