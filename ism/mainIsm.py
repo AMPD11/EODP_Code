d1754a47fa11e29296375dede9b9dc0896f00ed2
AMPD11 <andreimariuspopd@gmail.com>
2025-11-05 21:54:03 +0100

    EODP-TS-E2E-0001. TLS Module directories config on mainism and mainl1b.

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -6,7 +6,7 @@ from ism.src.ism import ism
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/eodpcode/auxiliary"
 indir  = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/data/EODP-TS-ISM/input/gradient_alt100_act150"
-outdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/ISM_0001"
+outdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/E2E_TLS/ISM"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/eodpcode/auxiliary"
-indir  = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/data/EODP-TS-L1B/input"
-outdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/L1B_0001"
+indir  = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/E2E_TLS/ISM"
+outdir = "C:/Users/Andrei/Documents/MiSE/3.1 Semester/Earth Observation Data Processing/EODP_Outputs/E2E_TLS/L1B"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
